2bab5166df1e19b3f3906e834a83d529fc3b18b0
pjt3591oo <pjt3591oo@gmail.com>
2021-12-08 12:30:03 +0900

    custom element 클래스 반환 함수생성, 이벤트 해제 로직 변경

--- index.js
+++ index.js
@@ -3,79 +3,82 @@ const state = [
   {last_name: "Ashburne2", first_name: "Deane2", address: "2 Green Place", state: false},
   {last_name: "Ashburne3", first_name: "Deane3", address: "3 Green Place", state: false},
 ]
-
-class TableBody extends HTMLElement {
-  select = -1;
-  state = [];
-
-  rowTemplate = document.getElementById('table-view-row');
-
-  constructor () {
-    super();
-    this.state = state;
-    this.render();
-  } 
-
-  render() {
-    for(let i = 0; i < this.state.length; i++) {
-      const rowTemplate = document.importNode(this.rowTemplate.content, true);
-      const row = rowTemplate.querySelector("tr");
-      const columns = row.querySelectorAll("td");
-
-      columns[0].appendChild(
-        this.makeInput(i)
-      );
-
-      columns[1].textContent = this.state[i].last_name;
-      columns[2].textContent = this.state[i].first_name;
-      columns[3].textContent = this.state[i].address;
-
-      this.querySelector('tbody').appendChild(row)
+function TableBody(_state) {
+  return class extends HTMLElement {
+    select = -1;
+    state = [];
+  
+    rowTemplate = document.getElementById('table-view-row');
+    inputEvents = []
+    constructor () {
+      console.log(_state)
+      super();
+      this.state = _state;
+      this.render();
+    } 
+  
+    render() {
+      for(let i = 0; i < this.state.length; i++) {
+        const rowTemplate = document.importNode(this.rowTemplate.content, true);
+        const row = rowTemplate.querySelector("tr");
+        const columns = row.querySelectorAll("td");
+  
+        columns[0].appendChild(
+          this.makeInput(i)
+        );
+  
+        columns[1].textContent = this.state[i].last_name;
+        columns[2].textContent = this.state[i].first_name;
+        columns[3].textContent = this.state[i].address;
+  
+        this.querySelector('tbody').appendChild(row)
+      }
+    }
+  
+    makeInput(idx) {
+      const input = document.createElement('input')
+      input.setAttribute("type", "radio");
+      input.setAttribute("name", "person");
+      input.addEventListener('change', this.onChangeHandler.bind(this, idx))
+      this.inputEvents.push(input);
+      return input;
+    }
+  
+    onChangeHandler(changedIdx, e) {
+      this.setAttribute('select', changedIdx);
+    }
+  
+    attributeChangedCallback(name, oldIdx, newIdx) {
+      if (oldIdx === null) return;
+      console.log(name, oldIdx, newIdx);
+      if (this.select > -1) this.state[this.select].state = false
+      this.select = parseInt(newIdx);
+      if (this.select > -1) this.state[this.select].state = true;
+      dialogShow(this.state[this.select]);
+    }
+  
+    disconnectedCallback() {
+      // 이벤트 바인딩 해제
+      this.inputEvents.forEach(input => input.removeEventListener(this.onChangeHandler));
+      console.log('disconnectedCallback');
+    }
+  
+  
+    static get observedAttributes() {
+      return ['select'];
     }
   }
-
-  makeInput(idx) {
-    const input = document.createElement('input')
-    input.setAttribute("type", "radio");
-    input.setAttribute("name", "person");
-    input.addEventListener('change', this.onChangeHandler.bind(this, idx))
-    return input;
-  }
-
-  onChangeHandler(changedIdx, e) {
-    this.setAttribute('select', changedIdx);
-    
-  }
-
-  attributeChangedCallback(name, oldIdx, newIdx) {
-    if (oldIdx === null) return;
-    console.log(name, oldIdx, newIdx);
-    if (this.select > -1) this.state[this.select].state = false
-    this.select = parseInt(newIdx);
-    if (this.select > -1) this.state[this.select].state = true;
-    dialogShow(this.state[this.select]);
-  }
-
-  disconnectedCallback() {
-    // TODO: addEventListener를 해제하는 코드를 넣어야 한다.
-    console.log('disconnectedCallback');
-  }
-
-
-  static get observedAttributes() {
-    return ['select'];
+  
+  function dialogShow(obj) {
+    const dialog = document.getElementById('dialog');
+    dialog.setAttribute('open', true);
+    dialog.innerHTML = `
+      <h1>${obj.last_name}</h1>
+      <h1>${obj.first_name}</h1>
+      <h1>${obj.address}</h1>
+      <button onclick="document.getElementById('dialog').close()">X</button>
+    `
   }
 }
 
-function dialogShow(obj) {
-  const dialog = document.getElementById('dialog');
-  dialog.setAttribute('open', true);
-  dialog.innerHTML = `
-    <h1>${obj.last_name}</h1>
-    <h1>${obj.first_name}</h1>
-    <h1>${obj.address}</h1>
-    <button onclick="document.getElementById('dialog').close()">X</button>
-  `
-}
-
-customElements.define('check-table', TableBody);
+customElements.define('check-table', TableBody(state));
